310b004b4645abd6f8e9fb092469f2a05a325a1a
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2026-05-15 17:22:44 -0700

    add meta tags for seo, responsive behavior, etc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ export class Home extends HTMLElement {
       <x-row>
           <x-column>
               <h1>Welcome!</h1>
-              <p>You've found a blog documenting the adventures of four related companions who's names all happen to start with J. Secretly this is dev project for big daddy J to get more accustomed with JS Web Components, but don't that stop from enjoying all this great content! :)</p>
+              <p>You've found a blog documenting the adventures of four related companions who's names all happen to start with J. Secretly this is dev project for big daddy J to get more accustomed with JS Web Components, but don't that stop from enjoying all this great content!</p>
           </x-column>
       </x-row>
     </${tagName(FullWidth)}>
